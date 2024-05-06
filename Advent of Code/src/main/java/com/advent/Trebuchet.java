0f2acda27073dd981153bc92c2996480ca5ca54c
JustinGianelloni <Justin.Gianelloni@gmail.com>
2024-05-06 00:36:30 -0400

    Part 2
    (Still incorrect)

--- Advent of Code/src/main/java/com/advent/Trebuchet.java
+++ Advent of Code/src/main/java/com/advent/Trebuchet.java
@@ -4,31 +4,72 @@
 package com.advent;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Trebuchet {
 
+    static final Map<String, Integer> words = Map.of(
+            "one", 1,
+            "two", 2,
+            "three", 3,
+            "four", 4,
+            "five", 5,
+            "six", 6,
+            "seven", 7,
+            "eight", 8,
+            "nine", 9);
+
     public static void main(String[] args) throws Exception {
         URL resource = Trebuchet.class.getClassLoader().getResource("trebuchet.txt");
         File file = new File(resource.toURI());
+        int part1 = parsePage(file, 1);
+        System.out.println("Part 1 Answer: " + part1);
+        int part2 = parsePage(file, 2);
+        System.out.println("Part 2 Answer: " + part2);
+    }
+
+    private static int getNumber(char[] chars) {
+        ArrayList<Character> digits = new ArrayList<>();
+        for (char c : chars) {
+            if (Character.isDigit(c)) {
+                digits.add(c);
+            }
+        }
+        char[] result = {digits.getFirst(), digits.getLast()};
+        return Integer.parseInt(new String(result));
+    }
+
+    private static int parsePage(File file, int part) throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
-        int answer = 0;
+        int number = 0;
         while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            char[] chars = line.toCharArray();
-            ArrayList<Character> digits = new ArrayList<Character>();
-            for (char c : chars) {
-                if (Character.isDigit(c)) {
-                    digits.add(c);
+            number += parseLine(scanner.nextLine(), part);
+        }
+        return number;
+    }
+
+    private static int parseLine(String line, int part) {
+        if (part == 2) {
+            line = replaceWords(line.toLowerCase());
+        }
+        return getNumber(line.toCharArray());
+    }
+
+    private static String replaceWords(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            for (Map.Entry<String, Integer> word : words.entrySet()) {
+                if (line.indexOf(word.getKey()) == i) {
+                    System.out.println("Line: " + line);
+                    System.out.println("Replacing " + word.getKey() + " with: " + word.getValue());
+                    line = line.replaceFirst(word.getKey(), word.getValue().toString());
+                    System.out.println("New Line: " + line + "\n");
                 }
             }
-            char[] comb = {digits.getFirst(), digits.getLast()};
-            int num = Integer.parseInt(new String(comb));
-            System.out.println("Adding " + num + " to " + answer + ": " + (num + answer));
-            answer += num;
         }
-        scanner.close();
+        return line;
     }
 }
