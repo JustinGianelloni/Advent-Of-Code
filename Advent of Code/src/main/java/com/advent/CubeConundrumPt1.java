20d893dac035de0f7cf35f58e34c0ed650feccff
JustinGianelloni <Justin.Gianelloni@gmail.com>
2024-05-06 16:55:32 -0400

    Day 2 Solution

---
+++ Advent of Code/src/main/java/com/advent/CubeConundrumPt1.java
@@ -0,0 +1,79 @@
+//Solution for 2023-2 Advent of Code
+//https://adventofcode.com/2023/day/2
+
+package com.advent;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class CubeConundrumPt1 {
+
+    private enum Colors {
+        red(12),
+        green(13),
+        blue(14);
+
+        private final int max;
+
+        Colors(final int color) {
+            this.max = color;
+        }
+
+        public int getMax() {
+            return max;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        URL resource = Trebuchet.class.getClassLoader().getResource("cube_conundrum.txt");
+        File file = new File(resource.toURI());
+        int answer = parsePage(file);
+        System.out.println("The answer is: " + answer);
+    }
+
+    private static int parsePage(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        int number = 0;
+        while (scanner.hasNextLine()) {
+            number += parseLine(scanner.nextLine());
+        }
+        return number;
+    }
+
+    private static int parseLine(String line) {
+        String[] split = line.split(": ");
+        String[] game = split[0].split(" ");
+        if (isGamePossible(split[1])) {
+            return Integer.parseInt(game[1]);
+        }
+        return 0;
+    }
+
+    private static boolean isGamePossible(String game) {
+        for (String reveal : game.split("; ")) {
+            if (!isRevealPossible(reveal)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isRevealPossible(String reveal) {
+        for (String color : reveal.split(", ")) {
+            if (!isColorPossible(color)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isColorPossible(String color) {
+        String[] split = color.split(" ");
+        if (Integer.parseInt(split[0]) > Colors.valueOf(split[1]).getMax()) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ Advent of Code/src/main/java/com/advent/CubeConundrumPt2.java
@@ -0,0 +1,57 @@
+package com.advent;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CubeConundrumPt2 {
+
+    public static void main(String[] args) throws Exception {
+        URL resource = Trebuchet.class.getClassLoader().getResource("cube_conundrum.txt");
+        File file = new File(resource.toURI());
+        int answer = parsePage(file);
+        System.out.println("The answer is: " + answer);
+    }
+
+    private static int parsePage(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        int number = 0;
+        while (scanner.hasNextLine()) {
+            number += parseLine(scanner.nextLine());
+        }
+        return number;
+    }
+
+    private static int parseLine(String line) {
+        String[] game = line.split(": ");
+        String[] reveals = game[1].split("; ");
+        HashMap<String, Integer> colorCount = getInitialColors();
+        for (String reveal : reveals) {
+            parseReveal(colorCount, reveal);
+        }
+        return colorCount.get("red") * colorCount.get("green") * colorCount.get("blue");
+    }
+
+    private static void parseReveal(HashMap<String, Integer> colorCount, String reveal) {
+        for (String color : reveal.split(", ")) {
+            parseColor(colorCount, color);
+        }
+    }
+
+    private static void parseColor(HashMap<String, Integer> colorCount, String color) {
+        String[] split = color.split(" ");
+        if (Integer.parseInt(split[0]) > colorCount.get(split[1])) {
+            colorCount.put(split[1], Integer.valueOf(split[0]));
+        }
+    }
+
+    private static HashMap<String, Integer> getInitialColors() {
+        HashMap<String, Integer> colors = new HashMap<>();
+        colors.put("red", 0);
+        colors.put("green", 0);
+        colors.put("blue", 0);
+        return colors;
+    }
+}
